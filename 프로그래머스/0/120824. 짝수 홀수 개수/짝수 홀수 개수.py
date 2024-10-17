4a314c89bd24d0540e4f6409e5d34e46f0d4939a
Yeom-Yeom <68320670+Yeom-Yeom@users.noreply.github.com>
2024-10-17 13:02:47 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,8 +1,5 @@
 def solution(num_list):
     ans = [0,0]
     for n in num_list:
-        if n%2==0:
-            ans[0]+=1
-        else:
-            ans[1]+=1
+        ans[n%2]+=1
     return ans
