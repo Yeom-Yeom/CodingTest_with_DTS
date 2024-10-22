b0b0bf105e1a622887b7209df23b4898370a06e2
Yeom-Yeom <68320670+Yeom-Yeom@users.noreply.github.com>
2024-10-22 13:04:20 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -2,24 +2,25 @@ from math import ceil
 
 def solution(progresses, speeds):
     answer = []
-    days = []  # 각 작업이 끝나기까지 남은 일수를 저장할 리스트
+    days = []  # 각 작업이 남은 일수 저장 리스트
     
     # 각 작업의 남은 일수를 계산
+    # ceil을 통해 소수점 올림. 소수점이 존재한다면 하루 더 걸린다는 것.
     for i in range(len(progresses)):
-        remaining_days = ceil((100 - progresses[i]) / speeds[i])
-        days.append(remaining_days)
+        tmp = ceil((100 - progresses[i]) / speeds[i])
+        days.append(tmp)
     
     # 첫 번째 작업이 끝나기까지 걸리는 일수를 기준으로 설정
-    current_deploy = days[0]
+    cur_day = days[0]
     count = 0  # 현재 배포에서 처리할 기능 개수
     
     for day in days:
-        if day <= current_deploy:
+        if day <= cur_day:
             count += 1  # 현재 기능이 함께 배포 가능하면 count 증가
         else:
-            # 새로운 배포 시작
+            # 새로운 배포
             answer.append(count)  # 지금까지 함께 배포된 기능 수 추가
-            current_deploy = day  # 새로운 기준 일수로 변경
+            cur_day = day  # 새로운 기준 일수로 변경
             count = 1  # 새롭게 시작하는 배포는 1개로 시작
     
     answer.append(count)  # 마지막으로 남은 배포 추가
