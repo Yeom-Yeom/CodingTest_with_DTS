0f86852abf14e964affc88aa12fd1d25eacac40b
Yeom-Yeom <diting9813@skuniv.ac.kr>
2024-11-03 01:08:13 +0900

    24.11.03

--- Zip.py
+++ Zip.py
@@ -1,8 +1,10 @@
 def solution(msg):
     alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-    dict = {k:v for (k,v) in zip(alphabet,list(range(1,27)))}
-    # (key,value) dictionary 를 zip함수를 통해 k=alphabet, v = 1~26
+    
+    # {'A':1,'B':2...'Z':26}
+    for i in range(len(alphabet)):
+        dict[alphabet[i]]= i+1
     
     ans = []
     while True:
@@ -17,4 +19,9 @@ def solution(msg):
                 break
     return ans
 
-# msg가 dictionary에 있으면
+# msg가 dictionary에 있으면 ans에 dict[msg]에 해당하는 번호 추가
+# 없다면, 1부터 msg의 길이까지
+# 만약 앞에서 부터 i번째 까지 dict에 없다면
+# ans에 추가,해당 번호는 length+1 
+# break로 for문 종료
+# 모든 문자가 존재한다면 while문 break
