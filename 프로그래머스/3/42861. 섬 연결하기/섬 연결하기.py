06a96886dbc9c1a4b4674c6791cdbe64c3c912b3
Yeom-Yeom <68320670+Yeom-Yeom@users.noreply.github.com>
2024-11-19 18:00:46 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,36 +1,15 @@
-# 노드가 포함되어 있는지 찾는 함수
-def find_node(sets, node):
-    for s in sets:
-        if node in s:
-            return s
-    return None
-
-
 def solution(n, costs):
-    costs.sort(key=lambda x: x[2])
-
-    # 각 섬을 set을 포함한 list로 초기화
-    sets = [set([i]) for i in range(n)]
-    total_cost = 0
-
-
-    for node1, node2, cost in costs:
-        # 각 섬이 속한 노드 찾기 이때 n1, n2는 sets의 원소를 참조 해서 저장
-        # 포인트의 개념?
-        n1 = find_node(sets, node1)
-        n2 = find_node(sets, node2)
-
-
-        # n1, n2 중 하나만 존재할 때
-        if n1 != n2:
-            n1.update(n2) # n1에 n2를 업데이트 하면 sets의 원소도 바뀜
-            sets.remove(n2) # n1에 n2를 포함 시키고 개별 원소로 존재하는 n2 제거
-            total_cost += cost
-
-            print("---------------------------")
-
-        # 모든 섬이 하나의 집합으로 통합되었으면 종료
-        if len(sets) == 1:
-            break
-
-    return total_cost
+    ans = 0
+    costs.sort(key=lambda x:x[2]) # 경로 비용이 적은 순으로 정렬
+    link = set([costs[0][0]]) # 시작점
+    
+    # kruskal 알고리즘
+    while len(link) != n: # 모든 위치가 연결될 때 까지
+        for v in costs:
+            if v[0] in link and v[1] in link: # 두 섬이 이미 낮은 비용으로 연결되었을 경우 무시
+                continue
+            if v[0] in link or v[1] in link: # 두 섬 중 하나가 현결되어있지 않을 때
+                link.update([v[0],v[1]]) # 섬 연결 후
+                ans += v[2] # 비용 더하기
+                break
+    return ans
