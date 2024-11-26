c5fd5c37ed0bb1ddad3972d2c38f400e5cec7012
Yeom-Yeom <diting9813@skuniv.ac.kr>
2024-11-26 10:08:31 +0900

    24.11.26

--- ranking.py
+++ ranking.py
@@ -6,6 +6,7 @@ def solution(n, results):
     for k in range(1,n+1): # 경유 노드
         for i in range(1,n+1): # 출발 노드
             for j in range(1,n+1): # 도착 노드
+                # 만약 i가 k를 이기고, k가 j를 이긴다면 i는 j를 이길 수 있음
                 if graph[i][k] and graph[k][j]: 
                     graph[i][j] = True
     
@@ -16,9 +17,12 @@ def solution(n, results):
         for j in range(1,n+1):
             if graph[i][j] or graph[j][i]: # 둘 중 하나가 이겼으면
                 cnt+=1
-        if cnt == n-1: # 다른 모든 선수와 승패가 정해진 경우
+        if cnt == n-1: # 자신을 제외한 다른 모든 선수와 승패가 정해진 경우
             answer +=1
     return answer
 
+# graph[i][j] 는 i가 j를 이길 수 있음을 의미.
+# 모든 선수에 대해 자신을 제외한 n-1명과 승패가 확정된 경우 순위를 알 수 있음.
 # 플로이드-워셜 알고리즘
 # https://velog.io/@kimdukbae/%ED%94%8C%EB%A1%9C%EC%9D%B4%EB%93%9C-%EC%9B%8C%EC%85%9C-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-Floyd-Warshall-Algorithm
+# n의 갯수가 100까지로 O(n^3) 이더라도, 큰 시간이 걸리지 않음.
--- shared_taxi_fare.py
+++ shared_taxi_fare.py
@@ -10,17 +10,21 @@ def solution(n,s,a,b,fares):
         graph[i][i] = 0
 
     # 주어진 요금 정보를 그래프에 반영
+    # fares 배열을 통해 직접 연결된 노드 간의 비용을 그래프에 반영
     for x,y,z in fares:
         graph[x][y] = z
         graph[y][x] = z
 
     # 2. 플로이드-워셜 알고리즘 실행
+    # 모든 쌍 최단 경로를 구하는 알고리즘
+    # k=경유지, i=출발지, j=도착지
     for k in range(1,n+1):
         for i in range(1,n+1):
             for j in range(1,n+1):
                 graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
     
     # 3. 최소 비용 계산
+    # 경유를 해서 갔을 때와, 그냥 갔을 때의 최소 비용을 계산
     min_cost = INF
     for t in range(1,n+1):
         cost = graph[s][t] + graph[t][a]+graph[t][b]
